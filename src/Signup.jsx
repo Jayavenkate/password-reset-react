fd7fb91a03f9fbfb572d212f989925dd39674688
Jayavenkate <jayajaikithuja@gmail.com>
2023-05-09 12:46:05 +0300

    signup

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -2,6 +2,7 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { useFormik } from "formik";
 import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
 
 const formValidationSchema = yup.object({
   username: yup.string().required("required"),
@@ -9,17 +10,25 @@ const formValidationSchema = yup.object({
   password: yup.string().required("password required").min(6),
 });
 export function Signup() {
+  const navigate= useNavigate();
   const { values, handleBlur, handleChange, handleSubmit, touched, errors } =
     useFormik({
       initialValues: {
-        
         username: "",
         email: "",
         password: "",
       },
       validationSchema: formValidationSchema,
-      onSubmit: (values) => {
+      onSubmit: async (values) => {
         console.log(values);
+        const data = await fetch("http://localhost:5000/signup", {
+          method: "POSt",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify(values),
+        });
+        const result = await data.json();
+        console.log(data);
+        navigate("/login");
       },
     });
   return (
